740994d82d434a48d5cc49baf22067c84572c5e7
Waliullah Muntasir <waliullah.muntasir@gmail.com>
2024-12-26 21:12:12 +0600

    ido many thing

---
+++ js.js
@@ -0,0 +1,27 @@
+
+// function fan(){
+//     console.log("rhtugurtuthughh")
+//     console.log("rhtugurtuthughh")
+//     console.log("rhtugurtuthughh")
+// }
+// fan()
+// function fanon(){
+//     console.log("rhtugurtuthughh")
+//     console.log("rhtugurtuthughh")
+//     console.log("rhtugurtuthughh")
+// }
+
+// fanon()
+
+
+
+// function fanon(sum1,sum2){
+//     console.log(sum1 + sum2)
+// }
+// fanon(10000000000,3467709887654233)
+
+function fanon(){
+    console.log("rhtugurtuthughh")
+    console.log("rhtugurtuthughh")
+    console.log("rhtugurtuthughh")
+}
